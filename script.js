510b8535ced0d8c93c639844d716180223628873
Vladimirpb1 <73517288+Vladimirpb1@users.noreply.github.com>
2020-10-27 15:57:56 +0300

    Calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+let num1; 
+let num2; 
+
+
+let out = document.getElementById("out");
+
+
+function plus() {
+	num1 = document.getElementById("n1").value;
+	num1 = parseInt(num1);
+	num2 = document.getElementById("n2").value;
+	num2 = parseInt(num2); 
+	out.innerHTML = num1 + num2;
+}
+buttonPlus.addEventListener("click",plus);
